2cf41833dc0f9c56822064cc17aba7eb571efddd
MiguelRomeral <miguel.romeral@gmail.com>
2018-05-02 00:33:47 +0200

    Tratango algunos detalles.
    Arreglado la primera asignacion del tablero (lo hacia mal).
    Tratados argumetnos en el main.

--- dosmil/juego.py
+++ dosmil/juego.py
@@ -1,33 +1,41 @@
+# -*- coding: utf-8 -*-    
 '''
 Created on 1 may. 2018
 
 @author: miguelangel.garciar
 '''
 from random import randint, random
+import sys
 from __builtin__ import True
-
-# Posibles valores de las casillas.
-VALORES = (2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
-           32768, 65536, 131072, 262144, 524288, 1048576)
-
-
-# Posibles tamaños de tablero.
-TABLEROS = (3, 4, 5, 6)
-
-# Probabilidad de que aparezca un 4 en lugar de un 2.
-PR_CUATRO = 0.15
 
 class Juego(int):
     '''
     Clase en la que se encuentra el tablero.
     '''
     
+    # Posibles valores de las casillas.
+    VALORES = (None, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
+               32768, 65536, 131072, 262144, 524288, 1048576)
+    
+    
+    # Posibles tamaï¿½os de tablero.
+    TABLEROS = (3, 4, 5, 6, 7, 8)
+    
+    # Probabilidad de que aparezca un 4 en lugar de un 2.
+    PR_CUATRO = 0.09
+    
     def __init__(self, longitud):
         '''
         Constructor del Tablero.
         '''
         self.longitud = longitud
-        self.tablero = [[None] * longitud] * longitud
+        columnas = []
+        for i in range(0, longitud):
+            fila = []
+            for j in range(0, longitud):
+                fila.append(None)
+            columnas.append(fila)
+        self.tablero = columnas
         self.puntuacion = 0
         
         
@@ -40,7 +48,7 @@ class Juego(int):
     
     def vacia(self, x, y):
         '''
-        Indica si una casilla esta vacia (antes habrá que comprobar las coordenadas con: valida(x,y).
+        Indica si una casilla esta vacia (antes habrï¿½ que comprobar las coordenadas con: valida(x,y).
         '''
         return self.tablero[x][y] == None
     
@@ -53,20 +61,50 @@ class Juego(int):
     
     
     def nueva_casilla(self): 
+        '''
+        Rellena una casilla con un 2 o 4 de forma aleatoria (si hay hueco). Retorna True si se pudo.
+        '''
         valida = False
+        contador = 200
         x = -1
         y = -1
-        while not valida:
+        while ((not valida) and (contador > 0)):
             x = self.aleatorio()
             y = self.aleatorio()
             if self.vacia(x,y):
                 valida = True
+            else:
+                contador-= 1
+        if contador == 0:
+            # Finalizara el juego, ya lo ha intentado suficientes veces.
+            return False
         # Valor a poner, un 2 o un 4.
         valor = -1
-        if random() < PR_CUATRO:
+        if random() < self.PR_CUATRO:
             valor = 4
         else:
             valor = 2
         self.tablero[x][y] = valor
+        return True
+        
+        
+    def imprimir(self):
+        print('-----------------------')
+        i = 0
+        while i < self.longitud:
+            for el in self.tablero[i]:
+                if el == None:
+                    sys.stdout.write(' - ')
+                else:
+                    #print(' {} '.format(el), end="\t")
+                    sys.stdout.write(' {} '.format(el))
+            print('')
+            i += 1
+        print('-----------------------')
+        return ''
+                
+        
+        
+        
         
         
--- dosmil/main.py
+++ dosmil/main.py
@@ -1,13 +1,55 @@
+# -*- coding: utf-8 -*-   
 '''
 Created on 1 may. 2018
 
 @author: miguelangel.garciar
 '''
+import dosmil.juego
+import sys
+from dosmil.juego import Juego
+from telnetlib import EL
+
+COM_HELP_1 = '-h'
+COM_HELP_2 = '--help'
+COM_SIZE_1 = '-s'
+COM_SIZE_2 = '--size'
+
+def usage():
+    print("""
+    Uso:
+        {} | {}        Muestra las opciones en la llamada.
+        {} | {}        Especifica el tama�o del tablero (entre 3 y 8). Por defecto: 4
+    """.format(COM_HELP_1, COM_HELP_2, COM_SIZE_1, COM_SIZE_2))
 
 if __name__ == '__main__':
-    '''
+    # Tratamos los argumentos:
+    tamanyo = 4
+    argu = None
+    del sys.argv[0] # Elimino la llamada al programa
+    for el in sys.argv:
+        if argu == None:
+            if el == COM_SIZE_1 or el == COM_SIZE_2:
+                argu = el
+            else:
+                argu = 'desconocido'
+        else:
+            if argu == COM_SIZE_1 or argu == COM_SIZE_2:
+                if int(el) in Juego.TABLEROS:
+                    tamanyo = int(el)
+                else:
+                    print('Ese tamaño de tablero no es valido:')
+                    usage()
+                    sys.exit()
+            else:
+                print('Opcion no reconocida.')
+                usage()
+                sys.exit()
+    # Creamos el juego:
+    game = Juego(tamanyo)
     
-    Y AHORA, LA PREVIA DE LA CHAMPIONS!!
-    
-    '''
-    pass
+    # PRuebas
+    game.nueva_casilla()
+    game.nueva_casilla()
+    game.nueva_casilla()
+    game.imprimir()
+    sys.exit()
