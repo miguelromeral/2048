ba148fd599654fc29e9327846425157af3498527
MiguelRomeral <miguel.romeral@gmail.com>
2018-05-01 23:27:47 +0200

    Mergeado

--- dosmil/juego.py
+++ dosmil/juego.py
@@ -10,6 +10,7 @@ from __builtin__ import True
 VALORES = (2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
            32768, 65536, 131072, 262144, 524288, 1048576)
 
+
 # Posibles tamaños de tablero.
 TABLEROS = (3, 4, 5, 6)
 
